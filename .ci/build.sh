6a8e18dea9506be085ec3914ee952ae1032b534d
Armin Ronacher <armin.ronacher@active-4.com>
2016-11-08 22:51:31 +0100

    Moved version printing

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ex
 
+rustc --version
+cargo --version
+
 rustup target add $TARGET || true
 cargo build --target $TARGET --release
--- .ci/install.sh
+++ .ci/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -ex
 curl https://static.rust-lang.org/rustup.sh | sh -s -- --prefix=$HOME/rust --spec=nightly-2016-08-10 --with-target=$TARGET
-rustc --version
-cargo --version
