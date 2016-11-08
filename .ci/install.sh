49c7f67489c857982157bddf435aa060bef74549
Armin Ronacher <armin.ronacher@active-4.com>
2016-11-08 22:45:18 +0100

    Fixed paths

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ex
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-rustup override set nightly-2016-10-08
+curl https://sh.rustup.rs -sSf | sh -s -- -y --prefix=$HOME/rust --spec=nightly-2016-10-08 --with-target=$TARGET
 rustc --version
 cargo --version
