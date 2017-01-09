fae04d031f6d0f70c0fa9480399455cac7d7674f
Armin Ronacher <armin.ronacher@active-4.com>
2017-01-09 10:39:02 +0100

    Make travis happy

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,7 +2,8 @@
 
 # Older travis OS X images might not have pip
 if ! hash pip 2> /dev/null; then
-  easy_install pip
+  easy_install --user requests==2.10.0
+else
+  pip install --user requests==2.10.0
 fi
-pip install --user requests==2.10.0
 python ./.ci/upload-release.py
