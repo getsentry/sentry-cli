b3708258868e6925ce46361c2ff095ecd8ebabdb
Armin Ronacher <armin.ronacher@active-4.com>
2017-01-09 10:25:08 +0100

    Install pip with easy_install if needed

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Older travis OS X images might not have pip
+if ! hash pip 2> /dev/null; then
+  easy_install pip
+fi
 pip install --user requests==2.10.0
 python ./.ci/upload-release.py
