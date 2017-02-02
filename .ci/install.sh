0fee742805f15f96cc2d8ea9efac6588e8881320
Armin Ronacher <armin.ronacher@active-4.com>
2017-02-02 20:57:31 +0100

    beta -> stable

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ex
-curl https://static.rust-lang.org/rustup.sh | sh -s -- --prefix=$HOME/rust --spec=beta --with-target=$TARGET
+curl https://static.rust-lang.org/rustup.sh | sh -s -- --prefix=$HOME/rust --with-target=$TARGET
