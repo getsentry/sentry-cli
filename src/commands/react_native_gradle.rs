3b5a8c8efb165b1a657f6d11025ec5cf787b9956
Armin Ronacher <armin.ronacher@active-4.com>
2017-03-29 20:31:29 +0200

    Disable gradle command for now

--- src/commands/react_native_gradle.rs
+++ src/commands/react_native_gradle.rs
@@ -1,4 +1,4 @@
-use clap::{App, ArgMatches};
+use clap::{App, ArgMatches, AppSettings};
 
 use prelude::*;
 use config::Config;
@@ -7,9 +7,10 @@ use utils::ArgExt;
 
 pub fn make_app<'a, 'b: 'a>(app: App<'a, 'b>) -> App<'a, 'b> {
     app.about("uploads react-native projects from within a gradle build step")
+        .setting(AppSettings::Hidden)
         .org_project_args()
 }
 
-pub fn execute<'a>(matches: &ArgMatches<'a>, config: &Config) -> Result<()> {
-    Ok(())
+pub fn execute<'a>(_matches: &ArgMatches<'a>, _config: &Config) -> Result<()> {
+    Err(Error::from("this command is currently not implemented"))
 }
