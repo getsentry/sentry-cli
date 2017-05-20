bef02f811776c6eb2022a817ff87b4d3efe178fd
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-20 09:54:50 +0200

    Added missing index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+var os = require('os');
+var childProcess = require('child_process');
+var pkgInfo = require('./package.json');
+
+let path = null;
+if (os.platform() === 'win32') {
+  path = __dirname + '\\bin\\sentry-cli.exe';
+} else {
+  path = __dirname + '/bin/sentry-cli';
+}
+
+exports.getConfigStatus = function() {
+  return new Promise(function(resolve, reject) {
+    childProcess.execFile(
+      path,
+      ['info', '--config-status-json'],
+      function(err, stdout, stderr) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(JSON.parse(stdout));
+      });
+  });
+};
+
+exports.getVersion = function() {
+  return pkgInfo.version;
+};
+
+exports.getPath = function() {
+  return path;
+};
