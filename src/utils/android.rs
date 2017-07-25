fc3604bae944af4f80f98b1c214fae50e4791be9
Brett Hoerner <brett@bretthoerner.com>
2017-07-25 15:51:56 -0500

    Write Proguard mappings even if upload is disabled, create parent directories.

--- src/utils/android.rs
+++ src/utils/android.rs
@@ -102,6 +102,7 @@ pub fn dump_proguard_uuids_as_properties<P: AsRef<Path>>(
         .map(|x| x.to_string())
         .join("|"));
 
+    p.as_ref().parent().map(|parent| fs::create_dir_all(parent));
     let mut f = fs::File::create(p.as_ref())?;
     java_properties::write(&mut f, &props)
         .map_err(|_| Error::from("Could not persist proguard UUID in properties file"))?;
