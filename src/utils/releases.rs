51e1f747024483512f510ac254fdf336badbaad7
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-10-04 12:42:12 +0200

    fix: Refactor and fix release name for android codepush command

--- src/utils/releases.rs
+++ src/utils/releases.rs
@@ -1,26 +1,25 @@
 use std::fs;
 use std::io::Read;
 use std::env;
+use std::path::PathBuf;
 
 use utils::vcs;
 use utils::xcode::InfoPlist;
 
-use regex::Regex;
-
 use prelude::*;
 
 
-fn get_xcode_release_name() -> Result<Option<String>> {
+pub fn get_xcode_release_name(plist: Option<InfoPlist>) -> Result<Option<String>> {
     // if we are executed from within xcode, then we can use the environment
     // based discovery to get a release name without any interpolation.
-    if let Some(plist) = InfoPlist::discover_from_env()? {
+    if let Some(plist) = plist.or(InfoPlist::discover_from_env()?) {
         return Ok(Some(plist.get_release_name()));
     }
 
     Ok(None)
 }
 
-fn infer_gradle_release_name() -> Result<Option<String>> {
+pub fn infer_gradle_release_name(path: Option<PathBuf>) -> Result<Option<String>> {
     // this is similar to utils::codepush::get_codepush_release
     lazy_static! {
         static ref APP_ID_RE: Regex = Regex::new(
@@ -30,24 +29,23 @@ fn infer_gradle_release_name() -> Result<Option<String>> {
     }
 
     let mut contents = String::new();
-    if let Ok(mut here) = env::current_dir() {
-        loop {
-            if_chain! {
-                if let Ok(build_md) = here.join("build.gradle").metadata();
-                if build_md.is_file();
-                if let Ok(app_md) = here.join("app/build.gradle").metadata();
-                if app_md.is_file();
-                if let Ok(mut f) = fs::File::open(here.join("app/build.gradle"));
-                if f.read_to_string(&mut contents).is_ok();
-                if let Some(app_id_caps) = APP_ID_RE.captures(&contents);
-                if let Some(version_caps) = VERSION_NAME_RE.captures(&contents);
-                then {
-                    return Ok(Some(format!("{}-{}", &app_id_caps[1], &version_caps[1])));
-                }
+    let mut here = path.map_or(env::current_dir()?, |p| p.into());
+    loop {
+        if_chain! {
+            if let Ok(build_md) = here.join("build.gradle").metadata();
+            if build_md.is_file();
+            if let Ok(app_md) = here.join("app/build.gradle").metadata();
+            if app_md.is_file();
+            if let Ok(mut f) = fs::File::open(here.join("app/build.gradle"));
+            if f.read_to_string(&mut contents).is_ok();
+            if let Some(app_id_caps) = APP_ID_RE.captures(&contents);
+            if let Some(version_caps) = VERSION_NAME_RE.captures(&contents);
+            then {
+                return Ok(Some(format!("{}-{}", &app_id_caps[1], &version_caps[1])));
             }
-            if !here.pop() {
-                break;
-            }
+        }
+        if !here.pop() {
+            break;
         }
     }
 
@@ -60,7 +58,7 @@ pub fn detect_release_name() -> Result<String> {
     // xcodebuild which does not exist anywhere but there.
     if_chain! {
         if cfg!(target_os="macos");
-        if let Some(release) = get_xcode_release_name()?;
+        if let Some(release) = get_xcode_release_name(None)?;
         then {
             return Ok(release)
         }
@@ -69,7 +67,7 @@ pub fn detect_release_name() -> Result<String> {
     // For android we badly parse gradle files.  We do this because most of the
     // time now people set the ids and versions in the gradle files instead of
     // the xml manifests.
-    if let Some(release) = infer_gradle_release_name()? {
+    if let Some(release) = infer_gradle_release_name(None)? {
         return Ok(release);
     }
 
