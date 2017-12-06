263e27264699aff3006e993bc06be6700f83a193
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-12-06 09:40:06 +0100

    feat: Add SentryCli js interface
    * feat: Add SentryCli js interface
    
    Rename package to @sentry/cli
    Add tests
    
    * fix: npm install && npm test
    
    * build: Add minium required Node version (v4.8.6 LTS)
    
    * test(jest): Fix jest configuration
    
    * ci: Add a test with minimum supported node version
    
    * test: Ignore the coverage folder
    
    * meta(git): Ignore the downloaded binary
    
    * feat(js): Resolve cyclic dependency
    
    * ci: Publish as scoped package with public access
    
    * meta: Add yarn log
    
    * feat(js): Compatibility with Node v4
    
    * feat: Add bash mock test script
    
    * fix(ci): Only sentry local download test
    
    * fix(ci): Only sentry local download test
    
    * fix: use strict in install script
    
    * meta(vscode): Add jest extension
    
    * feat: support for node 0.12
    
    * fix(ci): Don't run jest on 0.12
    
    * meta(git): Fix gitignore for Windows

---
+++ js/__tests__/cli.test.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+
+var SentryCli = require('../cli');
+
+describe('SentryCli', function() {
+  test('call sentry-cli --version', function() {
+    expect.assertions(1);
+    var cli = new SentryCli();
+    return cli.execute(['--version']).then(function() {
+      expect(true).toBe(true);
+    });
+  });
+
+  test('call sentry-cli with wrong command', function() {
+    expect.assertions(1);
+    var cli = new SentryCli();
+    return cli.execute(['fail']).catch(function(e) {
+      expect(e.message).toMatch('Command failed:');
+    });
+  });
+});
---
+++ js/cli.js
@@ -0,0 +1,96 @@
+'use strict';
+
+/* global Promise */
+
+var childProcess = require('child_process');
+var os = require('os');
+var path = require('path');
+var pkgInfo = require('../package.json');
+
+var DEFAULT_IGNORE = ['node_modules'];
+
+var binaryPath =
+  os.platform() === 'win32'
+    ? path.resolve(__dirname, '..\\bin\\sentry-cli.exe')
+    : path.resolve(__dirname, '../sentry-cli');
+
+function transformIgnore(ignore) {
+  if (Array.isArray(ignore)) {
+    return ignore
+      .map(function(value) {
+        return ['--ignore', value];
+      })
+      .reduce(function(acc, value) {
+        return acc.concat(value);
+      }, []);
+  }
+  return ['--ignore', ignore];
+}
+
+function SentryCli(configFile) {
+  this.env = {};
+  if (typeof configFile === 'string') this.env.SENTRY_PROPERTIES = configFile;
+}
+
+SentryCli.prototype.execute = function(args) {
+  var env = this.env;
+  return new Promise(function(resolve, reject) {
+    childProcess.execFile(SentryCli.getPath(), args, { env: env }, function(err, stdout) {
+      if (err) return reject(err);
+      // eslint-disable-next-line
+      console.log(stdout);
+      return resolve();
+    });
+  });
+};
+
+SentryCli.prototype.getConfigStatus = function() {
+  return this.execute(['info', '--config-status-json']);
+};
+
+SentryCli.prototype.createRelease = function(release) {
+  return this.execute(['releases', 'new', release]);
+};
+
+SentryCli.prototype.finalizeRelease = function(release) {
+  return this.execute(['releases', 'finalize', release]);
+};
+
+SentryCli.prototype.uploadSourceMaps = function(options) {
+  return Promise.all(
+    options.include.map(function(sourcemapPath) {
+      var command = [
+        'releases',
+        'files',
+        options.release,
+        'upload-sourcemaps',
+        sourcemapPath,
+        '--rewrite'
+      ];
+
+      if (options.ignoreFile) {
+        command = command.concat(['--ignore-file', options.ignoreFile]);
+      }
+
+      if (options.ignore) {
+        command = command.concat(transformIgnore(options.ignore));
+      }
+
+      if (!options.ignoreFile && !options.ignore) {
+        command = command.concat(transformIgnore(DEFAULT_IGNORE));
+      }
+
+      return this.execute(command);
+    })
+  );
+};
+
+SentryCli.getVersion = function() {
+  return pkgInfo.version;
+};
+
+SentryCli.getPath = function() {
+  return binaryPath;
+};
+
+module.exports = SentryCli;
