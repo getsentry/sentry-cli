ad16dc14e599ac4b0d612d98094a52d029cdba64
Philip Kirkbride <phk@2klic.com>
2017-12-09 08:57:50 -0500

    update based on feedback from @jan-auer

--- src/utils/enc.rs
+++ src/utils/enc.rs
@@ -2,6 +2,8 @@ use std::str;
 use std::borrow::Cow;
 
 use chardet::detect;
+use encoding::DecoderTrap;
+use encoding::label::encoding_from_whatwg_label;
 
 use prelude::*;
 
@@ -11,12 +13,17 @@ pub fn decode_unknown_string(bytes: &[u8]) -> Result<Cow<str>> {
     if let Ok(s) = str::from_utf8(bytes) {
         Ok(Cow::Borrowed(s))
     } else {
-        let (enc, confidence, _) = detect(bytes);
-        if confidence < 0.5 {
-            println_stderr!("unknown encoding for string");
-            return Err(ErrorKind::QuietExit(1).into());
-        } else {
-            Ok(Cow::Owned(enc))
+        let (label, confidence, _) = detect(bytes);
+        if_chain! {
+            if confidence >= 0.5;
+            if let Some(enc) = encoding_from_whatwg_label(&label);
+            if let Ok(s) = enc.decode(bytes, DecoderTrap::Replace);
+            then {
+                Ok(Cow::Owned(s))
+            } else {
+                println_stderr!("unknown encoding for string");
+                return Err(ErrorKind::QuietExit(1).into());
+            }
         }
     }
 }
