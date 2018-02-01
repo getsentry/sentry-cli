7f7a012a7614207ad07dcfeeae83acf37d56b2f7
Jan Michael Auer <account@jauer.org>
2018-02-01 14:23:52 +0100

    docs(js): Add JSDoc comments to all methods (#237)
    * docs(js): Add JSDoc comments to all methods
    
    * ref(js): Trim the return value of releases.proposeVersion

--- js/releases/index.js
+++ js/releases/index.js
@@ -2,38 +2,107 @@
 
 const helper = require('../helper');
 
+/**
+ * Default arguments for the `--ignore` option.
+ * @type {string[]}
+ */
 const DEFAULT_IGNORE = ['node_modules'];
-const SOURCEMAPS_OPTIONS = require('./options/uploadSourcemaps');
 
+/**
+ * Schema for the `upload-sourcemaps` command.
+ * @type {OptionsSchema}
+ */
+const SOURCEMAPS_SCHEMA = require('./options/uploadSourcemaps');
+
+/**
+ * Manages releases and release artifacts on Sentry.
+ * @namespace SentryReleases
+ */
 module.exports = {
+  /**
+   * Registers a new release with sentry.
+   *
+   * The given release name should be unique and deterministic. It can later be used to
+   * upload artifacts, such as source maps.
+   *
+   * @param {string} release Unique name of the new release.
+   * @returns {Promise} A promise that resolves when the release has been created.
+   * @memberof SentryReleases
+   */
   new(release) {
     return helper.execute(['releases', 'new', release]);
   },
 
+  /**
+   * Marks this release as complete. This should be called once all artifacts has been
+   * uploaded.
+   *
+   * @param {string} release Unique name of the release.
+   * @returns {Promise} A promise that resolves when the release has been finalized.
+   * @memberof SentryReleases
+   */
   finalize(release) {
     return helper.execute(['releases', 'finalize', release]);
   },
 
+  /**
+   * Creates a unique, deterministic version identifier based on the project type and
+   * source files. This identifier can be used as release name.
+   *
+   * @returns {Promise.<string>} A promise that resolves to the version string.
+   * @memberof SentryReleases
+   */
   proposeVersion() {
-    return helper.execute(['releases', 'propose-version']);
+    return helper
+      .execute(['releases', 'propose-version'])
+      .then(version => version && version.trim());
   },
 
+  /**
+   * Scans the given include folders for JavaScript source maps and uploads them to the
+   * specified release for processing.
+   *
+   * The options require an `include` array, which is a list of directories to scan.
+   * Additionally, it supports to ignore certain files, validate and preprocess source
+   * maps and define a URL prefix.
+   *
+   * @example
+   * await cli.releases.uploadSourceMaps(cli.releases.proposeVersion(), {
+   *   // required options:
+   *   include: ['build'],
+   *
+   *   // default options:
+   *   ignore: ['node_modules'],  // globs for files to ignore
+   *   ignoreFile: null,          // path to a file with ignore rules
+   *   rewrite: false,            // preprocess sourcemaps before uploading
+   *   sourceMapReference: true,  // add a source map reference to source files
+   *   stripPrefix: [],           // remove certain prefices from filenames
+   *   stripCommonPrefix: false,  // guess common prefices to remove from filenames
+   *   validate: false,           // validate source maps and cancel the upload on error
+   *   urlPrefix: '',             // add a prefix source map urls after stripping them
+   *   ext: ['js', 'map', 'jsbundle', 'bundle'],  // override file extensions to scan for
+   * });
+   *
+   * @param {string} release Unique name of the release.
+   * @param {object} options Options to configure the source map upload.
+   * @returns {Promise} A promise that resolves when the upload has completed successfully.
+   * @memberof SentryReleases
+   */
   uploadSourceMaps(release, options) {
     if (!options || !options.include) {
       throw new Error('options.include must be a vaild path(s)');
     }
 
-    return Promise.all(
-      options.include.map(sourcemapPath => {
-        const args = ['releases', 'files', release, 'upload-sourcemaps', sourcemapPath];
-        const newOptions = Object.assign({}, options);
+    const uploads = options.include.map(sourcemapPath => {
+      const newOptions = Object.assign({}, options);
+      if (!newOptions.ignoreFile && !newOptions.ignore) {
+        newOptions.ignore = DEFAULT_IGNORE;
+      }
 
-        if (!newOptions.ignoreFile && !newOptions.ignore) {
-          newOptions.ignore = DEFAULT_IGNORE;
-        }
+      const args = ['releases', 'files', release, 'upload-sourcemaps', sourcemapPath];
+      return helper.execute(helper.prepareCommand(args, SOURCEMAPS_SCHEMA, options));
+    });
 
-        return helper.execute(helper.prepareCommand(args, SOURCEMAPS_OPTIONS, options));
-      })
-    );
+    return Promise.all(uploads);
   },
 };
