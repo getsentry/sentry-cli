28c5c6d07cdef55efb3a4ee6dcc8c6105493f854
Jan Michael Auer <jan.auer@sentry.io>
2018-02-09 13:55:37 +0100

    feat(utils): Add a util for batched iteration

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -12,29 +12,29 @@ mod sourcemaps;
 mod system;
 mod ui;
 mod update;
+pub mod batch;
 pub mod cordova;
 pub mod dif;
 pub mod upload;
 pub mod vcs;
 pub mod xcode;
 
-pub use self::android::{AndroidManifest, dump_proguard_uuids_as_properties};
-pub use self::args::{ArgExt, validate_uuid, validate_seconds, validate_timestamp,
-                     validate_project, get_timestamp};
+pub use self::android::{dump_proguard_uuids_as_properties, AndroidManifest};
+pub use self::args::{get_timestamp, validate_project, validate_seconds, validate_timestamp,
+                     validate_uuid, ArgExt};
 pub use self::codepush::{get_codepush_package, get_react_native_codepush_release};
-pub use self::enc::{decode_unknown_string};
+pub use self::enc::decode_unknown_string;
 pub use self::formatting::{HumanDuration, Table, TableRow};
-pub use self::fs::{TempDir, TempFile, is_writable, set_executable_mode, is_zip_file,
-                   get_sha1_checksum, SeekRead};
+pub use self::fs::{is_writable, is_zip_file, set_executable_mode, SeekRead, TempDir, TempFile,
+                   get_sha1_checksum, get_sha1_checksums};
 pub use self::iter::invert_result;
 pub use self::logging::Logger;
 pub use self::releases::detect_release_name;
-pub use self::sourcemaps::{SourceMapProcessor, get_sourcemap_reference_from_headers};
-pub use self::system::{propagate_exit_status, is_homebrew_install,
-                       is_npm_install, expand_envvars, expand_vars,
-                       print_error, to_timestamp,
-                       run_or_interrupt, init_backtrace, get_model, get_family};
-pub use self::ui::{prompt_to_continue, prompt, capitalize_string,
-                   copy_with_progress, make_byte_progress_bar};
+pub use self::sourcemaps::{get_sourcemap_reference_from_headers, SourceMapProcessor};
+pub use self::system::{expand_envvars, expand_vars, get_family, get_model, init_backtrace,
+                       is_homebrew_install, is_npm_install, print_error, propagate_exit_status,
+                       run_or_interrupt, to_timestamp};
+pub use self::ui::{capitalize_string, copy_with_progress, make_byte_progress_bar, prompt,
+                   prompt_to_continue};
 pub use self::update::{can_update_sentrycli, get_latest_sentrycli_release,
                        run_sentrycli_update_nagger, SentryCliUpdateInfo};
