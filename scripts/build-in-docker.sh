8203373729ed2d2d757d80387ada999358751404
Anton Ovchinnikov <anton@tonyo.info>
2018-05-03 17:33:43 +0200

    fix: Fix asset deployment for Linux builds

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -7,5 +7,11 @@ docker run \
         -w ${BUILD_DIR} \
         -v `pwd`:${BUILD_DIR}:ro \
         -v `pwd`/target:${BUILD_DIR}/target \
+        -v $HOME/.cargo/registry:/root/.cargo/registry \
         -it ${DOCKER_IMAGE} \
         cargo build --release --target=${TARGET} --locked
+
+# Fix permissions for shared directories
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+sudo chown -R ${USER_ID}:${GROUP_ID} target/ $HOME/.cargo
