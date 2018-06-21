dbf2c20c1fcd5985487fe3c23c241f029a1309f5
Jan Michael Auer <jan.auer@sentry.io>
2018-06-21 13:47:15 +0200

    feat(react_native): Add a command for appcenter-cli

--- src/commands/react_native.rs
+++ src/commands/react_native.rs
@@ -6,6 +6,7 @@ use commands;
 macro_rules! each_subcommand {
     ($mac:ident) => {
         $mac!(react_native_gradle);
+        $mac!(react_native_appcenter);
         $mac!(react_native_codepush);
         #[cfg(target_os = "macos")]
         $mac!(react_native_xcode);
