c6d0b749fc180127671203cbf7157dc506b3831f
Anton Ovchinnikov <anton@tonyo.info>
2018-08-20 14:11:32 +0300

    feat: Use secured base Docker image for Linux builds

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-set -ex
+set -eux
 
+DOCKER_IMAGE="getsentry/rust-musl-cross:${DOCKER_TAG}"
 BUILD_DIR="/work"
 
+DOCKER_RUN_OPTS="
+  -w ${BUILD_DIR}
+  -v $(pwd):${BUILD_DIR}:ro
+  -v $(pwd)/target:${BUILD_DIR}/target
+  -v $HOME/.cargo/registry:/root/.cargo/registry
+  ${DOCKER_IMAGE}
+"
+
 docker run \
-        -w ${BUILD_DIR} \
-        -v `pwd`:${BUILD_DIR}:ro \
-        -v `pwd`/target:${BUILD_DIR}/target \
-        -v $HOME/.cargo/registry:/root/.cargo/registry \
-        -it ${DOCKER_IMAGE} \
-        cargo build --release --target=${TARGET} --locked
+  ${DOCKER_RUN_OPTS} \
+  cargo build --release --target=${TARGET} --locked
+
+# Smoke test
+env | grep SENTRY_ > .env
+docker run \
+  --env-file=.env \
+  ${DOCKER_RUN_OPTS} \
+  cargo run --release --target=${TARGET} -- releases list
 
 # Fix permissions for shared directories
 USER_ID=$(id -u)
