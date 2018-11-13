f5d11c559bd5f3ddf469abbaccf73b04c000f040
Anton Ovchinnikov <anton@tonyo.info>
2018-11-13 11:10:31 +0100

    ci: Fix PR builds from forks

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -16,12 +16,14 @@ docker run \
   ${DOCKER_RUN_OPTS} \
   cargo build --release --target=${TARGET} --locked
 
-# Smoke test
-env | grep SENTRY_ > .env
-docker run \
-  --env-file=.env \
-  ${DOCKER_RUN_OPTS} \
-  cargo run --release --target=${TARGET} -- releases list
+# Smoke test (but only when building from the same repo)
+if [[ "${SENTRY_AUTH_TOKEN?x}" != "" ]]; then
+  env | grep SENTRY_ > .env
+  docker run \
+    --env-file=.env \
+    ${DOCKER_RUN_OPTS} \
+    cargo run --release --target=${TARGET} -- releases list
+fi
 
 # Fix permissions for shared directories
 USER_ID=$(id -u)
