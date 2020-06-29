031effa40072538d5fe9037aa6925d20388d8c39
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-06-29 14:34:05 +0200

    test: command_releases_info

---
+++ tests/command_releases_info.rs
@@ -0,0 +1,78 @@
+use mockito::{mock, server_url};
+
+use assert_cmd::Command;
+use predicates::str::{contains, is_empty};
+use std::collections::HashMap;
+
+const ENDPOINT: &str = "/api/0/projects/wat-org/wat-project/releases/wat-release/";
+const VALID_RESPONSE: &str = r#"{"dateReleased":"2020-06-29T12:16:49.368667Z","newGroups":0,"commitCount":0,"url":null,"data":{},"lastDeploy":null,"deployCount":0,"dateCreated":"2020-06-29T11:36:59.612687Z","lastEvent":null,"version":"wat-release","firstEvent":null,"lastCommit":null,"shortVersion":"wat-release","authors":[],"owner":null,"versionInfo":{"buildHash":null,"version":{"raw":"wat-release"},"description":"wat-release","package":null},"ref":null,"projects":[{"name":"test","platform":"javascript","slug":"test","platforms":["javascript"],"newGroups":0,"id":1861017}]}"#;
+
+fn get_base_env() -> HashMap<String, String> {
+    let mut env = HashMap::new();
+    env.insert(String::from("SENTRY_URL"), server_url());
+    env.insert(String::from("SENTRY_AUTH_TOKEN"), String::from("lolnope"));
+    env.insert(String::from("SENTRY_ORG"), String::from("wat-org"));
+    env.insert(String::from("SENTRY_PROJECT"), String::from("wat-project"));
+    env
+}
+
+#[test]
+fn info_shows_release_details() {
+    let _server = mock("GET", ENDPOINT)
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(VALID_RESPONSE)
+        .create();
+
+    Command::cargo_bin("sentry-cli")
+        .unwrap()
+        .envs(get_base_env())
+        .arg("releases")
+        .arg("info")
+        .arg("wat-release")
+        .assert()
+        .success()
+        .stdout(contains(
+            "| Version      | wat-release                    |",
+        ))
+        .stdout(contains(
+            "| Date created | 2020-06-29 11:36:59.612687 UTC |",
+        ));
+}
+
+#[test]
+fn info_doesnt_print_output_with_quiet_flag() {
+    let _server = mock("GET", ENDPOINT)
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(VALID_RESPONSE)
+        .create();
+
+    Command::cargo_bin("sentry-cli")
+        .unwrap()
+        .envs(get_base_env())
+        .arg("releases")
+        .arg("info")
+        .arg("wat-release")
+        .arg("--quiet")
+        .assert()
+        .success()
+        .stdout(is_empty());
+}
+
+#[test]
+fn info_exits_if_no_release_found() {
+    let _server = mock("GET", ENDPOINT)
+        .with_status(404)
+        .with_header("content-type", "application/json")
+        .create();
+
+    Command::cargo_bin("sentry-cli")
+        .unwrap()
+        .envs(get_base_env())
+        .arg("releases")
+        .arg("info")
+        .arg("wat-release")
+        .assert()
+        .failure();
+}
