3efda07f4943b554e14a3ffdce97b92a4956e397
Jan Michael Auer <mail@jauer.org>
2021-02-04 13:05:18 +0100

    Don't vendor openssl on armv7

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -4,11 +4,14 @@ set -eux
 DOCKER_IMAGE="messense/rust-musl-cross:${DOCKER_TAG}"
 BUILD_DIR="/work"
 
+# TODO: Remove OPENSSL_NO_VENDOR once openssl-src >111.11.0 is released.
+
 DOCKER_RUN_OPTS="
   -w ${BUILD_DIR}
   -v $(pwd):${BUILD_DIR}:ro
   -v $(pwd)/target:${BUILD_DIR}/target
   -v $HOME/.cargo/registry:/root/.cargo/registry
+  -e ARMV7_UNKNOWN_LINUX_MUSLEABI_OPENSSL_NO_VENDOR=1
   ${DOCKER_IMAGE}
 "
 
