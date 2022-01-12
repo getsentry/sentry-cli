4c8300199566f600cd1c55c3edfe332869b11b21
Katie Byers <katie.byers@sentry.io>
2022-01-12 07:26:48 -0800

    chore(various): Small organization fixes (#1090)
    This moves some tests around and organizes the imports and exports for `helper.js`.
    
    No behavior changes.

--- js/__tests__/helper.test.js
+++ js/__tests__/helper.test.js
@@ -1,6 +1,8 @@
 /* eslint-env jest */
 
+const os = require('os');
 const path = require('path');
+
 const helper = require('../helper');
 
 const SOURCEMAPS_OPTIONS = require('../releases/options/uploadSourcemaps');
@@ -22,127 +24,134 @@ describe('SentryCli helper', () => {
     return helper.execute(['fail']).catch(e => expect(e.message).toMatch('Command failed:'));
   });
 
-  test('call prepare command add default ignore', () => {
-    const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
-
-    expect(helper.prepareCommand(command)).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-    ]);
+  test('getPath returns platform-appropriate path', () => {
+    const pattern = os.platform() === 'win32' ? /sentry-cli.exe$/ : /sentry-cli$/;
+    expect(helper.getPath()).toMatch(pattern);
   });
 
-  test('call prepare command with array option', () => {
-    const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
+  describe('`prepare` command', () => {
+    test('call prepare command add default ignore', () => {
+      const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
 
-    expect(
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { stripPrefix: ['node', 'app'] })
-    ).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--strip-prefix',
-      'node',
-      '--strip-prefix',
-      'app',
-    ]);
+      expect(helper.prepareCommand(command)).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+      ]);
+    });
 
-    // Should throw since it is no array
-    expect(() => {
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { stripPrefix: 'node' });
-    }).toThrow();
-  });
+    test('call prepare command with array option', () => {
+      const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
 
-  test('call prepare command with boolean option', () => {
-    const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
+      expect(
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { stripPrefix: ['node', 'app'] })
+      ).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--strip-prefix',
+        'node',
+        '--strip-prefix',
+        'app',
+      ]);
 
-    expect(
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { sourceMapReference: false })
-    ).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--no-sourcemap-reference',
-    ]);
+      // Should throw since it is no array
+      expect(() => {
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { stripPrefix: 'node' });
+      }).toThrow();
+    });
 
-    expect(
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { sourceMapReference: true })
-    ).toEqual(['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null']);
+    test('call prepare command with boolean option', () => {
+      const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
 
-    expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { rewrite: true })).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--rewrite',
-    ]);
+      expect(
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { sourceMapReference: false })
+      ).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--no-sourcemap-reference',
+      ]);
 
-    expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { rewrite: false })).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--no-rewrite',
-    ]);
+      expect(
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { sourceMapReference: true })
+      ).toEqual(['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null']);
 
-    expect(() => {
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { sourceMapReference: 'node' });
-    }).toThrow();
-  });
+      expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { rewrite: true })).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--rewrite',
+      ]);
 
-  test('call prepare command with string option', () => {
-    const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
+      expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { rewrite: false })).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--no-rewrite',
+      ]);
 
-    expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { ext: ['js'] })).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--ext',
-      'js',
-    ]);
+      expect(() => {
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { sourceMapReference: 'node' });
+      }).toThrow();
+    });
 
-    expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { urlPrefix: '~/' })).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--url-prefix',
-      '~/',
-    ]);
+    test('call prepare command with string option', () => {
+      const command = ['releases', 'files', 'release', 'upload-sourcemaps', '/dev/null'];
 
-    expect(
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { urlSuffix: '?hash=1337' })
-    ).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--url-suffix',
-      '?hash=1337',
-    ]);
+      expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { ext: ['js'] })).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--ext',
+        'js',
+      ]);
 
-    expect(
-      helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { ignoreFile: '/js.ignore' })
-    ).toEqual([
-      'releases',
-      'files',
-      'release',
-      'upload-sourcemaps',
-      '/dev/null',
-      '--ignore-file',
-      '/js.ignore',
-    ]);
+      expect(helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { urlPrefix: '~/' })).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--url-prefix',
+        '~/',
+      ]);
+
+      expect(
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { urlSuffix: '?hash=1337' })
+      ).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--url-suffix',
+        '?hash=1337',
+      ]);
+
+      expect(
+        helper.prepareCommand(command, SOURCEMAPS_OPTIONS, { ignoreFile: '/js.ignore' })
+      ).toEqual([
+        'releases',
+        'files',
+        'release',
+        'upload-sourcemaps',
+        '/dev/null',
+        '--ignore-file',
+        '/js.ignore',
+      ]);
+    });
   });
 });
