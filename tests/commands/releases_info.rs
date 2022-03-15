feef38449ecccb3935f6ba6e19bd676ad52521ea
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-03-15 12:01:09 +0100

    Add tests

--- tests/commands/releases_info.rs
+++ tests/commands/releases_info.rs
@@ -20,9 +20,48 @@ fn shows_release_details() {
         .assert()
         .success()
         .stdout(
-            contains("| Version      | wat-release                    |").and(contains(
-                "| Date created | 2020-06-29 11:36:59.612687 UTC |",
-            )),
+            contains("| Version     | Date created                   |")
+                .and(contains("| wat-release | 2020-06-29 11:36:59.612687 UTC |")),
+        );
+}
+
+#[test]
+fn shows_release_details_with_projects_and_commits() {
+    let _server = mock("GET", "/api/0/projects/wat-org/wat-project/releases/wat-release/")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(r#"{"dateReleased":"2020-06-29T12:16:49.368667Z","newGroups":0,"commitCount":0,"url":null,"data":{},"lastDeploy":null,"deployCount":0,"dateCreated":"2020-06-29T11:36:59.612687Z","lastEvent":null,"version":"wat-release","firstEvent":null,"lastCommit":null,"shortVersion":"wat-release","authors":[],"owner":null,"versionInfo":{"buildHash":null,"version":{"raw":"wat-release"},"description":"wat-release","package":null},"ref":null,"projects":[{"name":"test","platform":"javascript","slug":"test","platforms":["javascript"],"newGroups":0,"id":1861017}]}"#)
+        .create();
+
+    let _commits = mock(
+        "GET",
+        "/api/0/projects/wat-org/wat-project/releases/wat-release/commits/",
+    )
+    .with_status(200)
+    .with_header("content-type", "application/json")
+    .with_body(r#"[{"id":"iddqd"},{"id":"idkfa"}]"#)
+    .create();
+
+    Command::cargo_bin("sentry-cli")
+        .unwrap()
+        .envs(common::get_base_env())
+        .args(vec![
+            "releases",
+            "info",
+            "wat-release",
+            "--show-commits",
+            "--show-projects",
+        ])
+        .assert()
+        .success()
+        .stdout(
+            contains("| Version     | Date created                   | Projects | Commits |")
+                .and(contains(
+                    "| wat-release | 2020-06-29 11:36:59.612687 UTC | test     | iddqd   |",
+                ))
+                .and(contains(
+                    "|             |                                |          | idkfa   |",
+                )),
         );
 }
 
