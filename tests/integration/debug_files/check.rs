bf63b711e2a38a5686a57c6490f156291d9ccb0c
Brad Kotsopoulos <bkotsopoulos@snapchat.com>
2022-04-22 13:04:32 -0400

    test: debug-files check test for wasm

--- tests/integration/debug_files/check.rs
+++ tests/integration/debug_files/check.rs
@@ -4,3 +4,8 @@ use crate::integration::register_test;
 fn command_debug_files_check() {
     register_test("debug_files/debug_files-check.trycmd");
 }
+
+#[test]
+fn command_debug_files_check_wasm() {
+    register_test("debug_files/debug_files-check-wasm.trycmd");
+}
