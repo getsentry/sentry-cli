c00ad960d9207c09ee33600acaedaaed4626b61e
Kryštof Woldřich <krystof.woldrich@sentry.io>
2022-10-03 13:12:14 +0200

    Add windows version of the test

--- tests/integration/debug_files/check.rs
+++ tests/integration/debug_files/check.rs
@@ -7,5 +7,8 @@ fn command_debug_files_check() {
 
 #[test]
 fn command_debug_files_check_no_file_allow_failure() {
+    #[cfg(not(windows))]
     register_test("debug_files/debug_files-check-no-file.trycmd");
+    #[cfg(windows)]
+    register_test("debug_files/debug_files-check-no-file-windows.trycmd");
 }
