88116a94eb18a24aa5400d2bb096ce8da548ca97
Kryštof Woldřich <krystof.woldrich@sentry.io>
2022-10-03 12:56:15 +0200

    Add flag test

--- tests/integration/debug_files/check.rs
+++ tests/integration/debug_files/check.rs
@@ -4,3 +4,8 @@ use crate::integration::register_test;
 fn command_debug_files_check() {
     register_test("debug_files/debug_files-check.trycmd");
 }
+
+#[test]
+fn command_debug_files_check_no_file_allow_failure() {
+    register_test("debug_files/debug_files-check-no-file.trycmd");
+}
