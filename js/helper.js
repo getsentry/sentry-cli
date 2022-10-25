90cc3d285c0c979450f78a2dbf7eb0fbd3d54b08
Tim Fish <tim@timfish.uk>
2022-10-25 16:46:53 +0200

    Add test and use path.resolve to demonstrate failing test

--- js/helper.js
+++ js/helper.js
@@ -6,16 +6,11 @@ const childProcess = require('child_process');
  * Absolute path to the sentry-cli binary (platform dependent).
  * @type {string}
  */
-let binaryPath = eval(
-  "require('path').resolve(__dirname, require('os').platform() === 'win32' ? '..\\sentry-cli.exe' : '../sentry-cli')"
+let binaryPath = require('path').resolve(
+  __dirname,
+  '..',
+  process.platform === 'win32' ? 'sentry-cli.exe' : 'sentry-cli'
 );
-
-/**
- * NOTE: `eval` usage is a workaround for @vercel/nft detecting the binary itself as the hard dependency
- *       and effectively always including it in the bundle, which is not what we want.
- * ref: https://github.com/getsentry/sentry-javascript/issues/3865
- * ref: https://github.com/vercel/nft/issues/203
- */
 
 /**
  * Overrides the default binary path with a mock value, useful for testing.
