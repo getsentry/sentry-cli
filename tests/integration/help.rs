75ac5e6d04388176490f032f404282585b241260
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-02-22 15:00:03 +0100

    fix: help command test

--- tests/integration/help.rs
+++ tests/integration/help.rs
@@ -2,5 +2,8 @@ use crate::integration::register_test;
 
 #[test]
 fn command_help() {
+    #[cfg(not(windows))]
     register_test("help/help.trycmd");
+    #[cfg(windows)]
+    register_test("help/help-windows.trycmd");
 }
