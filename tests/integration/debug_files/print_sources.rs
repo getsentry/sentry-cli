6e71be792db9acc63cefd4d744fa8b17ef6af61c
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2023-03-06 11:11:17 +0100

    fix: include sources referenced but not embedded by an object file (#1486)

--- tests/integration/debug_files/print_sources.rs
+++ tests/integration/debug_files/print_sources.rs
@@ -9,3 +9,8 @@ fn command_debug_files_print_sources_ppdb() {
 fn command_debug_files_print_sources_dll_embedded_ppdb_with_sources() {
     register_test("debug_files/debug_files-print_sources-dll-embedded-ppdb-with-sources.trycmd");
 }
+
+#[test]
+fn command_debug_files_print_sources_mixed_embedded_sources() {
+    register_test("debug_files/debug_files-print_sources-mixed-embedded-sources.trycmd");
+}
