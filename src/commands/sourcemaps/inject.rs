c9e5eea97d3f17b88c61d2951280524dee720ad1
Sebastian Zivota <loewenheim@users.noreply.github.com>
2023-03-15 20:54:52 +0100

    ref: Allow multiple paths in sourcemaps inject (#1523)
    Co-authored-by: Kamil Ogórek <kamil.ogorek@gmail.com>

--- src/commands/sourcemaps/inject.rs
+++ src/commands/sourcemaps/inject.rs
@@ -17,10 +17,11 @@ pub fn make_command(command: Command) -> Command {
             that id is used instead.",
         )
         .arg(
-            Arg::new("path")
-                .value_name("PATH")
-                .required(true)
-                .help("The path to the javascript files."),
+            Arg::new("paths")
+                .value_name("PATHS")
+                .num_args(1..)
+                .action(ArgAction::Append)
+                .help("A path to recursively search for javascript files that should be processed."),
         )
         .arg(
             Arg::new("dry_run")
@@ -33,14 +34,20 @@ pub fn make_command(command: Command) -> Command {
 
 pub fn execute(matches: &ArgMatches) -> Result<()> {
     let mut processor = SourceMapProcessor::new();
-    let path: PathBuf = matches.get_one::<String>("path").unwrap().into();
+
+    let paths = matches
+        .get_many::<String>("paths")
+        .unwrap()
+        .map(PathBuf::from);
     let dry_run = matches.get_flag("dry_run");
 
-    let search = ReleaseFileSearch::new(path);
-    let sources = search.collect_files()?;
-    for source in sources {
-        let url = path_as_url(&source.path);
-        processor.add(&url, source)?;
+    for path in paths {
+        let search = ReleaseFileSearch::new(path);
+        let sources = search.collect_files()?;
+        for source in sources {
+            let url = path_as_url(&source.path);
+            processor.add(&url, source)?;
+        }
     }
 
     processor.inject_debug_ids(dry_run)?;
