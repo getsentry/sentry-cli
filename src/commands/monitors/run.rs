df4aaf1c7c53e2720f9f0d3430f91b2ae0e81efa
Kamil Ogórek <kamil.ogorek@gmail.com>
2023-06-05 21:39:27 +0200

    Refactor some code to separate functions

--- src/commands/monitors/run.rs
+++ src/commands/monitors/run.rs
@@ -1,6 +1,6 @@
 use log::warn;
 use std::process;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use uuid::Uuid;
 
 use anyhow::Result;
@@ -8,6 +8,7 @@ use clap::{Arg, ArgAction, ArgMatches, Command};
 use console::style;
 
 use sentry::protocol::{MonitorCheckIn, MonitorCheckInStatus};
+use sentry::types::Dsn;
 
 use crate::api::{Api, ApiCreateMonitorCheckIn, ApiMonitorCheckInStatus, ApiUpdateMonitorCheckIn};
 use crate::config::Config;
@@ -46,8 +47,118 @@ pub fn make_command(command: Command) -> Command {
         )
 }
 
-pub fn execute(matches: &ArgMatches) -> Result<()> {
+fn run_program(args: Vec<&String>, monitor_slug: &str) -> (bool, Option<i32>, Duration) {
+    let started = Instant::now();
+    let mut p = process::Command::new(args[0]);
+    p.args(&args[1..]);
+    p.env("SENTRY_MONITOR_SLUG", monitor_slug);
+
+    let (success, code) = match p.status() {
+        Ok(status) => (status.success(), status.code()),
+        Err(err) => {
+            eprintln!(
+                "{} could not invoke program '{}': {}",
+                style("error").red(),
+                args[0],
+                err
+            );
+            (false, None)
+        }
+    };
+
+    let elapsed = started.elapsed();
+    (success, code, elapsed)
+}
+
+fn dsn_execute(
+    dsn: Dsn,
+    args: Vec<&String>,
+    monitor_slug: &str,
+    environment: &str,
+) -> (bool, Option<i32>) {
+    let check_in_id = Uuid::new_v4();
+
+    let open_checkin = MonitorCheckIn {
+        check_in_id,
+        monitor_slug: monitor_slug.to_string(),
+        status: MonitorCheckInStatus::InProgress,
+        duration: None,
+        environment: Some(environment.to_string()),
+        monitor_config: None,
+    };
+
+    with_sentry_client(dsn.clone(), |c| c.send_envelope(open_checkin.into()));
+
+    let (success, code, elapsed) = run_program(args, monitor_slug);
+
+    let status = if success {
+        MonitorCheckInStatus::Ok
+    } else {
+        MonitorCheckInStatus::Error
+    };
+
+    let duration = Some(elapsed.as_secs_f64());
+
+    let close_checkin = MonitorCheckIn {
+        check_in_id,
+        monitor_slug: monitor_slug.to_string(),
+        status,
+        duration,
+        environment: Some(environment.to_string()),
+        monitor_config: None,
+    };
+
+    with_sentry_client(dsn, |c| c.send_envelope(close_checkin.into()));
+
+    (success, code)
+}
+
+fn token_execute(
+    args: Vec<&String>,
+    monitor_slug: &str,
+    environment: &str,
+) -> (bool, Option<i32>, Option<anyhow::Error>) {
     let api = Api::current();
+    let monitor_checkin = api.create_monitor_checkin(
+        &monitor_slug.to_owned(),
+        &ApiCreateMonitorCheckIn {
+            status: ApiMonitorCheckInStatus::InProgress,
+            environment: environment.to_string(),
+        },
+    );
+
+    let (success, code, elapsed) = run_program(args, monitor_slug);
+
+    match monitor_checkin {
+        Ok(checkin) => {
+            let status = if success {
+                ApiMonitorCheckInStatus::Ok
+            } else {
+                ApiMonitorCheckInStatus::Error
+            };
+
+            let duration = Some(elapsed.as_secs() * 1000 + u64::from(elapsed.subsec_millis()));
+
+            api.update_monitor_checkin(
+                &monitor_slug.to_owned(),
+                &checkin.id,
+                &ApiUpdateMonitorCheckIn {
+                    status: Some(status),
+                    duration,
+                    environment: Some(environment.to_string()),
+                },
+            )
+            .ok();
+        }
+        Err(e) => {
+            return (success, code, Some(e.into()));
+        }
+    }
+
+    (success, code, None)
+}
+
+pub fn execute(matches: &ArgMatches) -> Result<()> {
     let config = Config::current();
     let dsn = config.get_dsn().ok();
 
@@ -58,118 +169,24 @@ pub fn execute(matches: &ArgMatches) -> Result<()> {
         warn!("Please use DSN auth.");
     }
 
+    let args: Vec<_> = matches.get_many::<String>("args").unwrap().collect();
     let monitor_slug = matches.get_one::<String>("monitor_slug").unwrap();
     let environment = matches.get_one::<String>("environment").unwrap();
-    let allow_failure = matches.get_flag("allow_failure");
-
-    let args: Vec<_> = matches.get_many::<String>("args").unwrap().collect();
-
-    let exec_pgrm = || {
-        let started = Instant::now();
-        let mut p = process::Command::new(args[0]);
-        p.args(&args[1..]);
-        p.env("SENTRY_MONITOR_SLUG", monitor_slug);
-
-        let (success, code) = match p.status() {
-            Ok(status) => (status.success(), status.code()),
-            Err(err) => {
-                eprintln!(
-                    "{} could not invoke program '{}': {}",
-                    style("error").red(),
-                    args[0],
-                    err
-                );
-                (false, None)
-            }
-        };
-
-        let elapsed = started.elapsed();
-        (success, code, elapsed)
-    };
 
     let (success, code) = match dsn {
-        // Use envelope API when dsn is provided. This is the prefered way to create check-ins, the
-        // legacy API will be removed in the next major CLI version.
-        Some(dsn) => {
-            let check_in_id = Uuid::new_v4();
-
-            let open_checkin = MonitorCheckIn {
-                check_in_id,
-                monitor_slug: monitor_slug.to_string(),
-                status: MonitorCheckInStatus::InProgress,
-                duration: None,
-                environment: Some(environment.to_string()),
-                monitor_config: None,
-            };
-
-            with_sentry_client(dsn.clone(), |c| c.send_envelope(open_checkin.into()));
-
-            let (success, code, elapsed) = exec_pgrm();
-
-            let status = if success {
-                MonitorCheckInStatus::Ok
-            } else {
-                MonitorCheckInStatus::Error
-            };
-
-            let duration = Some(elapsed.as_secs_f64());
-
-            let close_checkin = MonitorCheckIn {
-                check_in_id,
-                monitor_slug: monitor_slug.to_string(),
-                status,
-                duration,
-                environment: Some(environment.to_string()),
-                monitor_config: None,
-            };
-
-            with_sentry_client(dsn, |c| c.send_envelope(close_checkin.into()));
-
-            (success, code)
-        }
+        // Use envelope API when dsn is provided. This is the prefered way to create check-ins,
+        // and the legacy API will be removed in the next major CLI version.
+        Some(dsn) => dsn_execute(dsn, args, monitor_slug, environment),
         // Use legacy API when DSN is not provided
         None => {
-            let monitor_checkin = api.create_monitor_checkin(
-                monitor_slug,
-                &ApiCreateMonitorCheckIn {
-                    status: ApiMonitorCheckInStatus::InProgress,
-                    environment: environment.to_string(),
-                },
-            );
-
-            let (success, code, elapsed) = exec_pgrm();
-
-            match monitor_checkin {
-                Ok(checkin) => {
-                    let status = if success {
-                        ApiMonitorCheckInStatus::Ok
-                    } else {
-                        ApiMonitorCheckInStatus::Error
-                    };
-
-                    let duration =
-                        Some(elapsed.as_secs() * 1000 + u64::from(elapsed.subsec_millis()));
-
-                    api.update_monitor_checkin(
-                        monitor_slug,
-                        &checkin.id,
-                        &ApiUpdateMonitorCheckIn {
-                            status: Some(status),
-                            duration,
-                            environment: Some(environment.to_string()),
-                        },
-                    )
-                    .ok();
-                }
-                Err(e) => {
-                    if allow_failure {
-                        print_error(&anyhow::Error::from(e));
-                    } else {
-                        return Err(e.into());
-                    }
+            let (success, code, err) = token_execute(args, monitor_slug, environment);
+            if let Some(e) = err {
+                if matches.get_flag("allow_failure") {
+                    print_error(&e);
+                } else {
+                    return Err(e);
                 }
             }
-
             (success, code)
         }
     };
