b3d9de4b926a23e016d12b077df6381984151387
Sebastian Zivota <loewenheim@mailbox.org>
2023-06-16 18:58:24 +0200

    failing test

--- src/utils/sourcemaps/inject.rs
+++ src/utils/sourcemaps/inject.rs
@@ -516,7 +516,7 @@ something else
 //# sourceMappingURL=fake1
 
   // some other comment
- "use strict"; rest of the line
+"use strict"; rest of the line
 'use strict';
 some line
 //# sourceMappingURL=fake2
@@ -533,9 +533,45 @@ something else"#;
 //# sourceMappingURL=fake1
 
   // some other comment
- "use strict"; rest of the line
+"use strict"; rest of the line
 'use strict';
 !function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="00000000-0000-0000-0000-000000000000")}catch(e){}}();
+some line
+//# sourceMappingURL=fake2
+something else
+//# debugId=00000000-0000-0000-0000-000000000000
+//# sourceMappingURL=real
+"#;
+
+        assert_eq!(std::str::from_utf8(&source).unwrap(), expected);
+    }
+
+    #[test]
+    fn test_fixup_js_file_fake_use_strict() {
+        let source = r#"#!/bin/node
+//# sourceMappingURL=fake1
+
+  // some other comment
+"use strict"; rest of the line
+(this.foo=this.bar||[]).push([[2],[function(e,t,n){"use strict"; […] }
+some line
+//# sourceMappingURL=fake2
+//# sourceMappingURL=real
+something else"#;
+
+        let debug_id = DebugId::default();
+
+        let mut source = Vec::from(source);
+
+        fixup_js_file(&mut source, debug_id).unwrap();
+
+        let expected = r#"#!/bin/node
+//# sourceMappingURL=fake1
+
+  // some other comment
+"use strict"; rest of the line
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="00000000-0000-0000-0000-000000000000")}catch(e){}}();
+(this.foo=this.bar||[]).push([[2],[function(e,t,n){"use strict"; […] }
 some line
 //# sourceMappingURL=fake2
 something else
