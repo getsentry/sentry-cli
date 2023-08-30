ab0049eca025584c7b27163dbe7d892059197047
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-08-30 12:10:30 +0200

    Fix tests

--- tests/integration/react_native/mod.rs
+++ tests/integration/react_native/mod.rs
@@ -2,43 +2,47 @@
 use crate::integration::register_test;
 
 #[test]
+#[cfg(target_os = "macos")]
 fn xcode_wrap_call_minimum() {
-    #[cfg(target_os = "macos")]
     register_test("react_native/xcode-wrap-call-minimum.trycmd");
-    assert_empty_sourcemap_report();
-    clean_up();
+    assert_empty_sourcemap_report("rn-sourcemap-report-minimum.json");
+    clean_up("rn-sourcemap-report-minimum.json");
 }
 
 #[test]
+#[cfg(target_os = "macos")]
 fn xcode_wrap_call_bundle() {
-    #[cfg(target_os = "macos")]
     register_test("react_native/xcode-wrap-call-bundle.trycmd");
-    assert_full_sourcemap_report();
-    clean_up();
+    assert_full_sourcemap_report("rn-sourcemap-report-bundle.json");
+    clean_up("rn-sourcemap-report-bundle.json");
 }
 
 #[test]
+#[cfg(target_os = "macos")]
 fn xcode_wrap_call_custom_bundle() {
-    #[cfg(target_os = "macos")]
     register_test("react_native/xcode-wrap-call-custom-bundle.trycmd");
-    assert_full_sourcemap_report();
-    clean_up();
+    assert_full_sourcemap_report("rn-sourcemap-report-custom-bundle.json");
+    clean_up("rn-sourcemap-report-custom-bundle.json");
 }
 
 #[test]
+#[cfg(target_os = "macos")]
 fn xcode_wrap_call_expo_export() {
-    #[cfg(target_os = "macos")]
     register_test("react_native/xcode-wrap-call-expo-export.trycmd");
-    assert_full_sourcemap_report();
-    clean_up();
+    assert_full_sourcemap_report("rn-sourcemap-report-expo-export.json");
+    clean_up("rn-sourcemap-report-expo-export.json");
 }
 
-fn clean_up() {
-    std::fs::remove_file("rn-sourcemap-report.json").unwrap();
+fn clean_up(
+  path: &str,
+) {
+    std::fs::remove_file(path).unwrap();
 }
 
-fn assert_full_sourcemap_report() {
-    let actual_code = std::fs::read_to_string("rn-sourcemap-report.json").unwrap();
+fn assert_full_sourcemap_report(
+  actual: &str,
+) {
+    let actual_code = std::fs::read_to_string(actual).unwrap();
     let expected_code = std::fs::read_to_string(
         "tests/integration/_fixtures/react_native/full-sourcemap-report.json.expected",
     )
@@ -47,8 +51,10 @@ fn assert_full_sourcemap_report() {
     assert_eq!(actual_code, expected_code);
 }
 
-fn assert_empty_sourcemap_report() {
-    let actual_code = std::fs::read_to_string("rn-sourcemap-report.json").unwrap();
+fn assert_empty_sourcemap_report(
+  actual: &str,
+) {
+    let actual_code = std::fs::read_to_string(actual).unwrap();
     let expected_code = std::fs::read_to_string(
         "tests/integration/_fixtures/react_native/empty-sourcemap-report.json.expected",
     )
