59dd1c3b4da82b6cdc54e9ff57c07a7727849640
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-09-12 22:22:12 +0200

    Add debug id reference test

--- tests/integration/sourcemaps/upload.rs
+++ tests/integration/sourcemaps/upload.rs
@@ -155,3 +155,10 @@ fn command_sourcemaps_upload_indexed_ram_bundle() {
         mock_common_upload_endpoints(ServerBehavior::Modern, Default::default());
     register_test("sourcemaps/sourcemaps-upload-indexed-ram-bundle.trycmd");
 }
+
+#[test]
+fn command_sourcemaps_upload_hermes_bundle_with_referencing_debug_id() {
+    let _upload_endpoints =
+        mock_common_upload_endpoints(ServerBehavior::Modern, Default::default());
+    register_test("sourcemaps/sourcemaps-upload-file-hermes-bundle-reference-debug-id.trycmd");
+}
