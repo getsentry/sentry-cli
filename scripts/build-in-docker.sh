e4318a88df63bb987e7e01633f7a29a882e88488
Sebastian Zivota <loewenheim@mailbox.org>
2023-11-07 16:07:26 +0100

    build: Vendor OpenSSL on ArmV7

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -9,7 +9,6 @@ DOCKER_RUN_OPTS="
   -v $(pwd):${BUILD_DIR}:ro
   -v $(pwd)/target:${BUILD_DIR}/target
   -v $HOME/.cargo/registry:/root/.cargo/registry
-  -e ARMV7_UNKNOWN_LINUX_MUSLEABI_OPENSSL_NO_VENDOR=1
   ${DOCKER_IMAGE}
 "
 
