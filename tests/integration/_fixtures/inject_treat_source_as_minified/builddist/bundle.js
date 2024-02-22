218971ac93371f127f3ac268e76c1b0d6cfb9f66
Zac Lovoy <zwlovoy@gmail.com>
2024-02-21 20:10:36 -0800

    Added --treat-source-as-minified flag to sourcemaps inject command

---
+++ tests/integration/_fixtures/inject_treat_source_as_minified/builddist/bundle.js
@@ -0,0 +1,5 @@
+var MyElement=class extends LitElement{constructor(){super(...arguments),this.isInitialized=!0}render(){return x`
+        <div>
+            <h1>My Element</h1>
+        </div>
+      `}};export{MyElement};
