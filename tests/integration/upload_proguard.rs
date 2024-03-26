c84efc18ceeabd1ec416c9ccde15c3f117bc5829
Arpad Borsos <arpad.borsos@sentry.io>
2024-03-26 14:42:11 +0100

    Remove references/calls to legacy Reprocessing

--- tests/integration/upload_proguard.rs
+++ tests/integration/upload_proguard.rs
@@ -15,31 +15,10 @@ fn command_upload_proguard() {
         )
         .with_response_body("[]"),
     );
-    let _reprocessing = mock_endpoint(
-        EndpointOptions::new(
-            "POST",
-            "/api/0/projects/wat-org/wat-project/reprocessing/",
-            200,
-        )
-        .with_response_body("[]"),
-    );
     register_test("upload_proguard/upload_proguard.trycmd");
 }
 
 #[test]
 fn command_upload_proguard_no_upload() {
     register_test("upload_proguard/upload_proguard-no-upload.trycmd");
-}
-
-#[test]
-fn command_upload_proguard_no_reprocessing() {
-    let _dsyms = mock_endpoint(
-        EndpointOptions::new(
-            "POST",
-            "/api/0/projects/wat-org/wat-project/files/dsyms/",
-            200,
-        )
-        .with_response_body("[]"),
-    );
-    register_test("upload_proguard/upload_proguard-no-reprocessing.trycmd");
 }
