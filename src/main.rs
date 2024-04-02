1209c07cdea0074a2b247569a68d0874f469a633
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-04-02 16:37:51 +0200

    docs: Remove inaccurate and unnecessary comments (#2007)
    The Sentry CLI is not a library, and we probably don't need a comment for our 1-line main function.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-//! This is the library that powers the `sentry-cli` tool.  The primary
-//! exported function is `main` which is directly invoked from the
-//! compiled binary that links against this library.
-
 mod api;
 mod commands;
 mod config;
 mod constants;
 mod utils;
 
-/// Executes the command line application and exits the process.
 pub fn main() {
     commands::main()
 }
