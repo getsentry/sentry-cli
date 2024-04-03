65bf997c7d8bd15fd895eae8d175b119b8d9b736
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-04-03 18:06:48 +0200

    modified tests for new API

--- src/api/pagination.rs
+++ src/api/pagination.rs
@@ -20,27 +20,27 @@ mod tests {
 
     #[test]
     fn test_pagination_empty_string() {
-        let pagination = Pagination::from("");
-        assert_eq!(pagination.next_cursor(), None);
+        let result = next_cursor("");
+        assert_eq!(result, None);
     }
 
     #[test]
     fn test_pagination_with_next() {
-        let pagination = Pagination::from(
+        let result = next_cursor(
             "<https://sentry.io/api/0/organizations/sentry/releases/?&cursor=100:-1:1>; \
             rel=\"previous\"; results=\"false\"; cursor=\"100:-1:1\", \
             <https://sentry.io/api/0/organizations/sentry/releases/?&cursor=100:1:0>; \
             rel=\"next\"; results=\"true\"; cursor=\"100:1:0\"",
         );
-        assert_eq!(pagination.next_cursor(), Some("100:1:0"));
+        assert_eq!(result, Some("100:1:0"));
     }
 
     #[test]
     fn test_pagination_without_next() {
-        let pagination = Pagination::from(
+        let result = next_cursor(
             "<https://sentry.io/api/0/organizations/sentry/releases/?&cursor=100:-1:1>; \
             rel=\"previous\"; results=\"false\"; cursor=\"100:-1:1\"",
         );
-        assert_eq!(pagination.next_cursor(), None);
+        assert_eq!(result, None);
     }
 }
