0f4ec87aa9a30a0ba0e12f1e50c9a11363b8e980
Elias Ram <elias.ram@sentry.io>
2024-04-26 12:35:07 +0200

    feat(parsing): Add clap parser using derive syntax
    Future commands (and existing ones by means of refactoring) can use the new derive parser to make the code easier to read, write, and modify, e.g., by keeping argument declarations and reads in sync.

---
+++ src/utils/derive_parser.rs
@@ -0,0 +1,27 @@
+use super::value_parsers::kv_parser;
+use crate::utils::auth_token::AuthToken;
+use clap::{command, value_parser, ArgAction::SetTrue, Parser, Subcommand};
+
+#[derive(Parser)]
+pub struct SentryCLI {
+    #[command(subcommand)]
+    pub command: SentryCLICommand,
+
+    #[arg(global=true, long="header", value_name="KEY:VALUE", value_parser=kv_parser, help="Custom headers that should be attached to all requests{n}in key:value format")]
+    pub headers: Vec<(String, String)>,
+
+    #[arg(global=true, long, value_parser=value_parser!(AuthToken), help="Use the given Sentry auth token")]
+    pub auth_token: Option<AuthToken>,
+
+    #[arg(global=true, ignore_case=true, long, value_parser=["trace", "debug", "info", "warn", "error"], help="Set the log output verbosity")]
+    pub log_level: Option<String>,
+
+    #[arg(global=true, action=SetTrue, visible_alias="silent", long, help="Do not print any output while preserving correct exit code. This flag is currently implemented only for selected subcommands")]
+    pub quiet: bool,
+
+    #[arg(global=true, action=SetTrue, long, hide=true, help="Always return 0 exit code")]
+    pub allow_failure: bool,
+}
+
+#[derive(Subcommand)]
+pub enum SentryCLICommand {}
---
+++ src/utils/value_parsers.rs
@@ -0,0 +1,9 @@
+use std::error::Error;
+
+/// Parse key:value pair from string, used as a value_parser for Clap arguments
+pub(crate) fn kv_parser(s: &str) -> Result<(String, String), Box<dyn Error + Send + Sync>> {
+    let pos = s
+        .find(':')
+        .ok_or_else(|| format!("`{s}` is missing a `:`"))?;
+    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+}
