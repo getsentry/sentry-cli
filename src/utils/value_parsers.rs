74a1aa79dfb828676d87200d8acb43ce0c3ef02e
Elias Ram <elias.ram@sentry.io>
2024-05-27 20:26:26 +0200

    update kv_parser

--- src/utils/value_parsers.rs
+++ src/utils/value_parsers.rs
@@ -2,11 +2,7 @@ use anyhow::{anyhow, Result};
 
 /// Parse key:value pair from string, used as a value_parser for Clap arguments
 pub fn kv_parser(s: &str) -> Result<(String, String)> {
-    let pos = s
-        .find(':')
-        .ok_or_else(|| anyhow!(format!("`{s}` is missing a `:`")))?;
-    Ok((
-        s[..pos].parse().expect("infallible"),
-        s[pos + 1..].parse().expect("infallible"),
-    ))
+    s.split_once(':')
+        .map(|(k, v)| (k.into(), v.into()))
+        .ok_or_else(|| anyhow!("`{s}` is missing a `:`"))
 }
