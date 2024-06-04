787a388e8cd56bbd29173c7e28d014eab932c667
elramen <158566966+elramen@users.noreply.github.com>
2024-06-04 20:26:32 +0200

    ref(api): Use EnvelopesApi instead of the sentry-core client (#2081)
    Use sentry-cli's envelopes client, `EnvelopesApi`, instead of sentry-cores's `Client` method `send_envelope`. This change enables debug logging and non-zero exit codes for transmission failures like the other commands.

--- tests/integration/monitors/run.rs
+++ tests/integration/monitors/run.rs
@@ -1,43 +1,52 @@
-use crate::integration::{mock_endpoint, register_test, EndpointOptions};
+use crate::integration::{self, EndpointOptions};
 
 #[test]
 fn command_monitors_run() {
+    let _server =
+        integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
     if cfg!(windows) {
-        register_test("monitors/monitors-run-win.trycmd");
+        integration::register_test("monitors/monitors-run-win.trycmd");
     } else {
-        register_test("monitors/monitors-run.trycmd");
+        integration::register_test("monitors/monitors-run.trycmd");
     }
 }
 
 #[test]
 fn command_monitors_run_token_auth() {
-    let _server = mock_endpoint(
+    let _server = integration::mock_endpoint(
         EndpointOptions::new("POST", "/api/0/monitors/foo-monitor/checkins/", 200)
             .with_response_file("monitors/post-monitors.json"),
     );
     if cfg!(windows) {
-        register_test("monitors/monitors-run-token-auth-win.trycmd").env("SENTRY_DSN", "");
+        integration::register_test("monitors/monitors-run-token-auth-win.trycmd")
+            .env("SENTRY_DSN", "");
     } else {
-        register_test("monitors/monitors-run-token-auth.trycmd").env("SENTRY_DSN", "");
+        integration::register_test("monitors/monitors-run-token-auth.trycmd").env("SENTRY_DSN", "");
     }
 }
 
 #[test]
 fn command_monitors_run_osenv() {
-    register_test("monitors/monitors-run-osenv.trycmd");
+    let _server =
+        integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
+    integration::register_test("monitors/monitors-run-osenv.trycmd");
 }
 
 #[test]
 fn command_monitors_run_environment() {
-    register_test("monitors/monitors-run-environment.trycmd");
+    let _server =
+        integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
+    integration::register_test("monitors/monitors-run-environment.trycmd");
 }
 
 #[test]
 fn command_monitors_run_environment_long() {
-    register_test("monitors/monitors-run-environment-long.trycmd");
+    let _server =
+        integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
+    integration::register_test("monitors/monitors-run-environment-long.trycmd");
 }
 
 #[test]
 fn command_monitors_run_help() {
-    register_test("monitors/monitors-run-help.trycmd");
+    integration::register_test("monitors/monitors-run-help.trycmd");
 }
