19d08fb0d13c49c31900774aaa216b257cab97a3
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2024-09-20 17:58:05 +0200

    ref(api): Separate `Deploy` struct into separate submodule (#2161)
    Also, use `Cow` in the struct to avoid allocations

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2359,30 +2359,6 @@ impl fmt::Display for Repo {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Deploy<'d> {
-    #[serde(rename = "environment")]
-    pub env: String,
-    pub name: Option<String>,
-    pub url: Option<String>,
-    #[serde(rename = "dateStarted")]
-    pub started: Option<DateTime<Utc>>,
-    #[serde(rename = "dateFinished")]
-    pub finished: Option<DateTime<Utc>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub projects: Option<Vec<Cow<'d, str>>>,
-}
-
-impl<'d> Deploy<'d> {
-    /// Returns the name of this deploy, defaulting to `"unnamed"`.
-    pub fn name(&self) -> &str {
-        match self.name.as_deref() {
-            Some("") | None => "unnamed",
-            Some(name) => name,
-        }
-    }
-}
-
 #[derive(Debug, Serialize, Clone)]
 pub struct PatchSet {
     pub path: String,
