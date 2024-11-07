e4c5aa8dd44c8680e9f17e249bd65b88b7efae98
Daniel Szoke <daniel.szoke@sentry.io>
2024-11-07 16:04:23 +0100

    test: batch login tests

--- tests/integration/login.rs
+++ tests/integration/login.rs
@@ -1,16 +1,6 @@
 use crate::integration::register_test;
 
 #[test]
-fn command_login_help() {
-    register_test("login/login-help.trycmd");
-}
-
-#[test]
 fn command_login() {
-    register_test("login/login.trycmd");
-}
-
-#[test]
-fn command_login_with_token() {
-    register_test("login/login-with-token.trycmd");
+    register_test("login/*.trycmd");
 }
