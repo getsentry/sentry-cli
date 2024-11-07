8e4fd78ed024615b5a4a150e48076461acef2748
Daniel Szoke <daniel.szoke@sentry.io>
2024-11-07 18:18:10 +0100

    test(organizations): Batch tests together

--- tests/integration/organizations.rs
+++ tests/integration/organizations.rs
@@ -3,7 +3,8 @@ use mockito::server_url;
 use crate::integration::{mock_endpoint, register_test, EndpointOptions};
 
 #[test]
-fn command_organizations_list() {
+fn command_organizations() {
+    // Mocks are for the organizations list command.
     let _server = mock_endpoint(
         EndpointOptions::new("GET", "/api/0/organizations/?cursor=", 200)
             .with_response_file("organizations/get-organizations.json"),
@@ -23,10 +24,5 @@ fn command_organizations_list() {
         EndpointOptions::new("GET", "/api/0/users/me/regions/", 200)
             .with_response_body(region_response),
     );
-    register_test("organizations/organizations-list.trycmd");
-}
-
-#[test]
-fn command_organizations_list_help() {
-    register_test("organizations/organizations-list-help.trycmd");
+    register_test("organizations/*.trycmd");
 }
