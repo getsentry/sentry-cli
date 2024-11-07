457991f7c1f742c7180d01985d9e0a29b3a054f5
Daniel Szoke <daniel.szoke@sentry.io>
2024-11-07 16:16:57 +0100

    test: batch token validation tests

--- tests/integration/token_validation.rs
+++ tests/integration/token_validation.rs
@@ -2,10 +2,5 @@ use crate::integration::register_test;
 
 #[test]
 fn test_warn_invalid_auth_token() {
-    register_test("token_validation/warn-invalid-auth-token.trycmd");
-}
-
-#[test]
-fn test_valid_auth_token() {
-    register_test("token_validation/valid-auth-token.trycmd");
+    register_test("token_validation/*.trycmd");
 }
