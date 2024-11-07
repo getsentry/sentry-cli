e551cdfb85f6b5db20ddfff49476ec7e9540ecf8
Daniel Szoke <daniel.szoke@sentry.io>
2024-11-07 16:20:08 +0100

    test: batch update tests

--- tests/integration/update.rs
+++ tests/integration/update.rs
@@ -1,11 +1,6 @@
 use crate::integration::register_test;
 
 #[test]
-fn command_update_help() {
-    register_test("update/update-help.trycmd");
-}
-
-#[test]
 fn command_update() {
-    register_test("update/update.trycmd");
+    register_test("update/*.trycmd");
 }
