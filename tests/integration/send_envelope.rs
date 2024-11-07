6998c43502f435e17bbda0e303f128dadbddf904
Daniel Szoke <daniel.szoke@sentry.io>
2024-11-07 15:51:15 +0100

    test(send-envelope): batch tests together

--- tests/integration/send_envelope.rs
+++ tests/integration/send_envelope.rs
@@ -3,25 +3,8 @@ use crate::integration;
 use super::EndpointOptions;
 
 #[test]
-fn command_send_envelope_help() {
-    integration::register_test("send_envelope/send_envelope-help.trycmd");
-}
-
-#[test]
-fn command_send_envelope_no_file() {
-    integration::register_test("send_envelope/send_envelope-no-file.trycmd");
-}
-
-#[test]
-fn command_send_envelope_file() {
+fn command_send_envelope() {
     let _server =
         integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
-    integration::register_test("send_envelope/send_envelope-file.trycmd");
-}
-
-#[test]
-fn command_send_envelope_with_logging() {
-    let _server =
-        integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
-    integration::register_test("send_envelope/send_envelope-file-log.trycmd");
+    integration::register_test("send_envelope/*.trycmd");
 }
