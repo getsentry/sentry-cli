365ba3aa8a8e7715dd730b57182f8faefe6cdeaa
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2024-11-07 16:14:38 +0100

    test: Batch org token tests together (#2229)

--- tests/integration/org_tokens.rs
+++ tests/integration/org_tokens.rs
@@ -1,31 +1,6 @@
 use crate::integration::register_test;
 
 #[test]
-fn org_token_url_mismatch() {
-    register_test("org_tokens/url-mismatch.trycmd");
-}
-
-#[test]
-fn org_token_org_mismatch() {
-    register_test("org_tokens/org-mismatch.trycmd");
-}
-
-#[test]
-fn org_token_url_match() {
-    register_test("org_tokens/url-match.trycmd");
-}
-
-#[test]
-fn org_token_org_match() {
-    register_test("org_tokens/org-match.trycmd");
-}
-
-#[test]
-fn org_token_url_works() {
-    register_test("org_tokens/url-works.trycmd");
-}
-
-#[test]
-fn org_token_url_mismatch_empty_token() {
-    register_test("org_tokens/url-mismatch-empty-token.trycmd");
+fn org_token() {
+    register_test("org_tokens/*.trycmd");
 }
