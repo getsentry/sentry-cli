553ca8da50b557a552832e37a7a166b02f05e351
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2024-11-07 18:24:26 +0100

    test(monitors): Batch monitors tests (#2236)

---
+++ tests/integration/monitors.rs
@@ -0,0 +1,34 @@
+use crate::integration::{mock_endpoint, register_test, EndpointOptions};
+
+#[test]
+fn command_monitors() {
+    let _list_endpoint = mock_endpoint(
+        EndpointOptions::new("GET", "/api/0/organizations/wat-org/monitors/?cursor=", 200)
+            .with_response_file("monitors/get-monitors.json"),
+    );
+    let _envelope_endpoint =
+        mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
+    let _token_endpoint = mock_endpoint(
+        EndpointOptions::new("POST", "/api/0/monitors/foo-monitor/checkins/", 200)
+            .with_response_file("monitors/post-monitors.json"),
+    );
+
+    let cases = register_test("monitors/*.trycmd");
+
+    #[cfg(not(windows))]
+    cases.case("tests/integration/_cases/monitors/not_windows/*.trycmd");
+
+    #[cfg(windows)]
+    cases.case("tests/integration/_cases/monitors/windows/*.trycmd");
+}
+
+#[test]
+fn command_monitors_run_server_error() {
+    let _server = mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 500));
+
+    #[cfg(not(windows))]
+    register_test("monitors/server_error/monitors-run-server-error.trycmd");
+
+    #[cfg(windows)]
+    register_test("monitors/server_error/monitors-run-server-error-win.trycmd");
+}
