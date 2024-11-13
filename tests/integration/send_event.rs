693d62167041846e2da823b7f3b0f21b673b5b1f
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2024-11-13 14:52:53 +0100

    test: batch send-event tests (#2230)

--- tests/integration/send_event.rs
+++ tests/integration/send_event.rs
@@ -1,29 +1,19 @@
 use crate::integration::{self, EndpointOptions};
 
-#[test]
-fn command_send_event_help() {
-    integration::register_test("send_event/send_event-help.trycmd");
-}
-
 // I have no idea why this is timing out on Windows.
 // I verified it manually, and this command works just fine. — Kamil
 // TODO: Fix windows timeout.
 #[cfg(not(windows))]
 #[test]
-fn command_send_event_raw() {
+fn command_send_event_not_windows() {
     let _server =
         integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
-    integration::register_test("send_event/send_event-raw.trycmd");
+    integration::register_test("send_event/not_windows/*.trycmd");
 }
 
 #[test]
-fn command_send_event_file() {
+fn command_send_event() {
     let _server =
         integration::mock_endpoint(EndpointOptions::new("POST", "/api/1337/envelope/", 200));
-    integration::register_test("send_event/send_event-file.trycmd");
-}
-
-#[test]
-fn command_send_event_raw_fail() {
-    integration::register_test("send_event/send_event-raw-fail.trycmd");
+    integration::register_test("send_event/*.trycmd");
 }
