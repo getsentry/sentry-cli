9cffc5f790233729759e1210dbe0f62978894ed4
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2025-06-04 20:26:24 -0700

    Wireup uploads

--- src/commands/mobile_app/upload.rs
+++ src/commands/mobile_app/upload.rs
@@ -8,9 +8,14 @@ use symbolic::common::ByteView;
 use zip::write::SimpleFileOptions;
 use zip::ZipWriter;
 
+use crate::api::Api;
+use crate::config::Config;
 use crate::utils::args::ArgExt;
+use crate::utils::chunks::{upload_chunks, Chunk, ASSEMBLE_POLL_INTERVAL};
+use crate::utils::fs::get_sha1_checksums;
 use crate::utils::fs::TempFile;
 use crate::utils::mobile_app::{is_aab_file, is_apk_file, is_xcarchive_directory, is_zip_file};
+use crate::utils::progress::ProgressBar;
 
 pub fn make_command(command: Command) -> Command {
     command
@@ -80,12 +85,48 @@ pub fn execute(matches: &ArgMatches) -> Result<()> {
         normalized_zips.push(normalized_zip);
     }
 
+    let api = Api::current();
+    let authenticated_api = api.authenticated()?;
+
+    let config = Config::current();
+    let (org, project) = config.get_org_and_project(matches)?;
+    let chunk_upload_options = authenticated_api
+        .get_chunk_upload_options(&org)?
+        .expect("Chunked uploading is not supported for this organization");
+
     for zip in normalized_zips {
-        println!("Created normalized zip at: {}", zip.path().display());
-        // TODO: Upload the normalized zip to the chunked uploads API
+        let progress_style = ProgressStyle::default_bar().template(&format!(
+            "{} Uploading files...\
+       \n{{wide_bar}}  {{bytes}}/{{total_bytes}} ({{eta}})",
+            style(">").dim(),
+        ));
+
+        let byteview = ByteView::open(zip.path())?;
+        let (checksum, checksums) =
+            get_sha1_checksums(&byteview, chunk_upload_options.chunk_size as usize)?;
+        let mut chunks = byteview
+            .chunks(chunk_upload_options.chunk_size as usize)
+            .zip(checksums.iter())
+            .map(|(data, checksum)| Chunk((*checksum, data)))
+            .collect::<Vec<_>>();
+
+        let response = authenticated_api
+            .assemble_mobile_app(&org, &project, checksum, &checksums, None, None)?;
+        chunks.retain(|Chunk((digest, _))| response.missing_chunks.contains(digest));
+
+        if !chunks.is_empty() {
+            upload_chunks(&chunks, &chunk_upload_options, progress_style)?;
+            println!("{} Uploaded files to Sentry", style(">").dim());
+        } else {
+            println!(
+                "{} Nothing to upload, all files are on the server",
+                style(">").dim()
+            );
+        }
+        poll_assemble(checksum, &checksums, &org, &project)?;
     }
 
-    eprintln!("Uploading mobile app files to a project is not yet implemented.");
+    // eprintln!("Uploading mobile app files to a project is not yet implemented.");
     Ok(())
 }
 
@@ -175,4 +216,45 @@ fn normalize_directory(path: &Path) -> Result<TempFile> {
         file_count
     );
     Ok(temp_file)
+}
+
+fn poll_assemble(checksum: Digest, checksums: &[Digest], org: &str, project: &str) -> Result<()> {
+    let progress_style = ProgressStyle::default_spinner().template("{spinner} Processing files...");
+
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(100);
+    pb.set_style(progress_style);
+    let api = Api::current();
+    let authenticated_api = api.authenticated()?;
+
+    let response = loop {
+        let response: crate::api::AssembleMobileAppResponse = authenticated_api
+            .assemble_mobile_app(&org, &project, checksum, &checksums, None, None)?;
+
+        if response.state.is_finished() {
+            break response;
+        }
+
+        std::thread::sleep(ASSEMBLE_POLL_INTERVAL);
+    };
+
+    if response.state.is_err() {
+        let message = response.detail.as_deref().unwrap_or("unknown error");
+        bail!("Failed to process uploaded files: {}", message);
+    }
+
+    pb.finish_with_duration("Processing");
+
+    if response.state.is_pending() {
+        println!(
+            "{} File upload complete (processing pending on server)",
+            style(">").dim()
+        );
+    } else {
+        println!("{} File processing complete", style(">").dim());
+    }
+
+    // print_upload_context_details(context);
+
+    Ok(())
 }
