8f0cf5ae001d835bc06de488bff6a9dec00c3e78
Vjeran Grozdanic <vjeran.grozdanic@sentry.io>
2025-08-13 15:16:22 +0200

    chore(metrics): Remove send_metric command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -39,7 +39,6 @@ mod releases;
 mod repos;
 mod send_envelope;
 mod send_event;
-mod send_metric;
 mod sourcemaps;
 #[cfg(not(feature = "managed"))]
 mod uninstall;
@@ -70,7 +69,6 @@ macro_rules! each_subcommand {
         $mac!(repos);
         $mac!(send_event);
         $mac!(send_envelope);
-        $mac!(send_metric);
         $mac!(sourcemaps);
         $mac!(dart_symbol_map);
         #[cfg(not(feature = "managed"))]
