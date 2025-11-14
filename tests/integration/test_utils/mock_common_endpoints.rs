741cd33b6d21814f984c6d650a3fa3b8fd1b7302
Daniel Szoke <daniel.szoke@sentry.io>
2025-11-14 14:47:24 +0100

    chore(sourcemaps): Remove support for release bundle uploads (#2961)
    ### Description
    Remove support for the deprecated release bundle uploads. Source maps are always uploaded as artifact bundles, now.
    
    Also, we now assume all Sentry servers support `ArtifactBundle` and `ArtifactBundleV2`.
    
    ### Issues
    - Resolves #2842
    - Resolves [CLI-188](https://linear.app/getsentry/issue/CLI-188/remove-support-for-release-bundle-uploads)

--- tests/integration/test_utils/mock_common_endpoints.rs
+++ tests/integration/test_utils/mock_common_endpoints.rs
@@ -1,4 +1,9 @@
-use std::fmt::Display;
+use std::{
+    fmt::Display,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+use serde_json::json;
 
 use crate::integration::test_utils::MockEndpointBuilder;
 
@@ -6,30 +11,11 @@ use crate::integration::test_utils::MockEndpointBuilder;
 /// These can be used to generate mocks for the upload endpoints.
 pub(super) fn common_upload_endpoints(
     server_url: impl Display,
-    behavior: ServerBehavior,
-    chunk_options: ChunkOptions,
+    chunk_size: Option<usize>,
+    initial_missing_chunks: Option<Vec<&'static str>>,
 ) -> impl Iterator<Item = MockEndpointBuilder> {
-    let ChunkOptions {
-        chunk_size,
-        missing_chunks,
-    } = chunk_options;
-    let (accept, release_request_count, assemble_endpoint) = match behavior {
-        ServerBehavior::Legacy => (
-            "\"release_files\"",
-            2,
-            "/api/0/organizations/wat-org/releases/wat-release/assemble/",
-        ),
-        ServerBehavior::Modern => (
-            "\"release_files\", \"artifact_bundles\"",
-            0,
-            "/api/0/organizations/wat-org/artifactbundle/assemble/",
-        ),
-        ServerBehavior::ModernV2 => (
-            "\"release_files\", \"artifact_bundles_v2\"",
-            0,
-            "/api/0/organizations/wat-org/artifactbundle/assemble/",
-        ),
-    };
+    let chunk_size = chunk_size.unwrap_or(8388608);
+    let assemble_endpoint = "/api/0/organizations/wat-org/artifactbundle/assemble/";
     let chunk_upload_response = format!(
         "{{
             \"url\": \"{server_url}/api/0/organizations/wat-org/chunk-upload/\",
@@ -38,46 +24,38 @@ pub(super) fn common_upload_endpoints(
             \"maxRequestSize\": 33554432,
             \"concurrency\": 8,
             \"hashAlgorithm\": \"sha1\",
-            \"accept\": [{accept}]
+            \"accept\": []
           }}",
     );
 
+    let is_first_request = AtomicBool::new(true);
+
     vec![
-        MockEndpointBuilder::new("POST", "/api/0/projects/wat-org/wat-project/releases/")
-            .with_status(208)
-            .with_response_file("releases/get-release.json")
-            .expect(release_request_count),
         MockEndpointBuilder::new("GET", "/api/0/organizations/wat-org/chunk-upload/")
             .with_response_body(chunk_upload_response),
         MockEndpointBuilder::new("POST", "/api/0/organizations/wat-org/chunk-upload/")
             .with_response_body("[]"),
         MockEndpointBuilder::new("POST", assemble_endpoint)
-            .with_response_body(format!(
-                r#"{{"state":"created","missingChunks":{}}}"#,
-                serde_json::to_string(&missing_chunks).unwrap()
-            ))
+            .with_response_fn(move |_| {
+                let response = if let Some(missing_chunks) = is_first_request
+                    .swap(false, Ordering::Relaxed)
+                    .then_some(initial_missing_chunks.as_ref())
+                    .flatten()
+                {
+                    json!({
+                        "state": "not_found",
+                        "missingChunks": missing_chunks,
+                    })
+                } else {
+                    json!({
+                        "state": "created",
+                        "missingChunks": [],
+                    })
+                };
+
+                serde_json::to_vec(&response).expect("failed to serialize response")
+            })
             .expect_at_least(1),
     ]
     .into_iter()
-}
-
-pub enum ServerBehavior {
-    Legacy,
-    Modern,
-    ModernV2,
-}
-
-#[derive(Debug)]
-pub struct ChunkOptions {
-    pub chunk_size: usize,
-    pub missing_chunks: Vec<String>,
-}
-
-impl Default for ChunkOptions {
-    fn default() -> Self {
-        Self {
-            chunk_size: 8388608,
-            missing_chunks: vec![],
-        }
-    }
 }
