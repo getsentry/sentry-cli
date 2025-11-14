3d010609cf1ab9364aa08cc37609b057d6c52d7a
Daniel Szoke <daniel.szoke@sentry.io>
2025-11-14 14:47:24 +0100

    chore(sourcemaps): Remove support for release bundle uploads (#2961)
    ### Description
    Remove support for the deprecated release bundle uploads. Source maps are always uploaded as artifact bundles, now.
    
    Also, we now assume all Sentry servers support `ArtifactBundle` and `ArtifactBundleV2`.
    
    ### Issues
    - Resolves #2842
    - Resolves [CLI-188](https://linear.app/getsentry/issue/CLI-188/remove-support-for-release-bundle-uploads)

--- tests/integration/react_native/xcode.rs
+++ tests/integration/react_native/xcode.rs
@@ -1,9 +1,9 @@
-use crate::integration::{ServerBehavior, TestManager};
+use crate::integration::TestManager;
 
 #[test]
 fn xcode_upload_source_maps_missing_plist() {
     TestManager::new()
-        .mock_common_upload_endpoints(ServerBehavior::Modern, Default::default())
+        .mock_common_upload_endpoints(None, None)
         .register_trycmd_test("react_native/xcode-upload-source-maps-invalid-plist.trycmd")
         .with_default_token();
 }
@@ -11,7 +11,7 @@ fn xcode_upload_source_maps_missing_plist() {
 #[test]
 fn xcode_upload_source_maps_release_and_dist_from_env() {
     TestManager::new()
-        .mock_common_upload_endpoints(ServerBehavior::Modern, Default::default())
+        .mock_common_upload_endpoints(None, Some(vec!["60f215dae7d29497357013d08c35e93716b6a46c"]))
         .register_trycmd_test(
             "react_native/xcode-upload-source-maps-release_and_dist_from_env.trycmd",
         )
