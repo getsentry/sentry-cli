ee866b7a9701741f6284206bb2f093b56b9556ce
Daniel Szoke <daniel.szoke@sentry.io>
2025-12-03 17:54:02 +0100

    feat(sourcemaps): Inject `debugId` into sourcemaps, not `debug_id` (#3005)
    Align debug ID injection [with the relevant TC39 proposal](https://github.com/tc39/ecma426/blob/main/proposals/debug-id.md#debug-ids-in-source-maps) by upgrading the `sourcemap` crate to [9.3.0](https://github.com/getsentry/rust-sourcemap/releases/tag/9.3.0), which serializes the Debug ID on a sourcemap [under the `"debugId"` key, instead of the `"debug_id"` key](https://github.com/getsentry/rust-sourcemap/pull/134). The crate still supports reading the `"debug_id"` key, but the `"debugId"` key takes precedence if both keys are present.
    
    These changes primarily affect the `sourcemaps inject` command, which will now place the debug ID in the `"debugId"` field rather than the `"debug_id"` field. The `sourcemaps upload` command is also affected, as sourcemaps will be rewritten with `"debugId"` rather than `"debug_id"`. Previously, rewriting preserved the key being used; if a sourcemap contained both keys, both would have been preserved after rewriting, but after these changes, we emit only `"debugId"`. For example, if a sourcemap contains only the `"debug_id"` key, the rewritten sourcemap will have that key in `"debugId"` instead; however, if a sourcemap contains both the `"debugId"` and the `"debug_id"` keys, the `"debug_id"` is completely stripped, and its value ignored.
    
    Closes #2221
    Closes [CLI-44](https://linear.app/getsentry/issue/CLI-44/time-capsule-dont-open-before-nov-6-2025-emit-debugid-instead-of-debug)

--- tests/integration/_expected_outputs/sourcemaps/sourcemaps-inject-embedded/dummy_embedded.js
+++ tests/integration/_expected_outputs/sourcemaps/sourcemaps-inject-embedded/dummy_embedded.js
@@ -1,4 +1,4 @@
 
 !function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="387af3ce-423a-5792-8d09-194d56a0ac3f")}catch(e){}}();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VSb290IjoiIiwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImRlYnVnX2lkIjoiMzg3YWYzY2UtNDIzYS01NzkyLThkMDktMTk0ZDU2YTBhYzNmIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VSb290IjoiIiwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImRlYnVnSWQiOiIzODdhZjNjZS00MjNhLTU3OTItOGQwOS0xOTRkNTZhMGFjM2YifQ==
 //# debugId=387af3ce-423a-5792-8d09-194d56a0ac3f
