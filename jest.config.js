ef659de18c43f40278af001382f48fe3b59c149f
Jan Peer Stöcklmair <jan.peer@sentry.io>
2026-03-09 12:34:43 +0100

    chore: upgrade Jest to v30 (#3191)
    Upgrades Jest to v30 and removes `@tootallnate/once`
    
    **Before:**
    <img width="357" height="149" alt="Screenshot 2026-03-09 at 09 08 12"
    src="https://github.com/user-attachments/assets/d18672de-5283-45ee-9189-ef896dd94cc5"
    />
    
    
    **After:** 
    <img width="366" height="54" alt="Screenshot 2026-03-09 at 09 06 47"
    src="https://github.com/user-attachments/assets/c59309f6-9c28-4fca-bf06-e5a3c2ec1e67"
    />

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  collectCoverage: true,
+  testEnvironment: 'node',
   setupFiles: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: ['<rootDir>/src/'],
+  testPathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/tests/integration/'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
