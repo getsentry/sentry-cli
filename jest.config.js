0f566d2ed6525c8991cb11f76c58c949215a5ec2
JPeer264 <jan.peer@sentry.io>
2026-03-10 10:46:07 +0100

    Revert "test: Resolves setupFiles instead of letting Jest handling it (#3201)"
    This reverts commit 0c4c6b38a50a980c7323cd606c94d27f7249c8ca.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverage: true,
   testEnvironment: 'node',
-  setupFiles: [require.resolve('./setupTests.js')],
+  setupFiles: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/tests/integration/'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
